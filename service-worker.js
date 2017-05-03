a3b31b0dd24293ba693b3a915f7dd0b7ed5b6eb8
Lee <leebee1234@hotmail.co.uk>
2017-05-03 23:22:32 +0100

    added

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
       console.log('ServiceWorker was successful);
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
