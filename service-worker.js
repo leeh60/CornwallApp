9a7e125de393d63fd271a0f252a458607ca7d260
Lee <leebee1234@hotmail.co.uk>
2017-05-04 17:36:39 +0100

    added

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 var CACHE_NAME = 'gih-cache';
 var CACHED_URLS = [
-  'index.html',
+  'offline.html',
   'styles.css',
   'padstow.jpg',
 ];
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
-      return caches.match('index.html');
+      return caches.match('offline.html');
     })
   );
 });
