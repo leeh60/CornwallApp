e2ef83bda70d5e7413debe3515ccf8c68b44b9f4
Lee <leebee1234@hotmail.co.uk>
2017-05-04 14:38:31 +0100

    added

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,28 @@
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+  'offline.html',
+  'styles.css’,
+  'padstow.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
-      return new Response(
-        'Welcome to the our service worker test \n'+        
-'There seems to be a problem with your connection.\n'+        
-'We look forward to telling you about Paddy as soon as you are online'
-      );
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
     })
   );
 });
