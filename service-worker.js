2d87e0d26c6af324333cd05106167b82677a8c13
Lee <leebee1234@hotmail.co.uk>
2017-05-04 17:06:12 +0100

    added

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,7 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return new Response(
-        'Welcome to the our service worker test \n'+        
-'There seems to be a problem with your connection.\n'+        
-'We look forward to telling you about Paddy as soon as you are online'
-      );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('gih-cache').then(function(cache) {
+      return cache.add('offline.html');
     })
   );
 });
