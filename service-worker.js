3e99aabec0d3387563a888d26ae085ee1a327e9d
Lee <leebee1234@hotmail.co.uk>
2017-05-04 13:50:28 +0100

    added

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', function(event) {
   if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
     event.respondWith(
       new Response('body { background: green; }',
-      { headers: { 'Content-Type': 'text/css' } }
+      { header: { 'Content-Type': 'text/css' } }
     ));
   }
 });
