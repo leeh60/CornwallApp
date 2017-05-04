dcbaee703f7403e69f314a7c3a8c22814e3b8b43
Lee <leebee1234@hotmail.co.uk>
2017-05-04 14:06:59 +0100

    added

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ if ('serviceWorker' in navigator) {
     console.log('Service worker registration failed: ', err);
   });
 }
+
+const askJack = document.getElementById("askJackForm");
+if(askJack){
+    const askJackFormSubmit = e => {
+        e.preventDefault();
+        const contact = {
+            "name": e.target.name.value,
+            "email": e.target.email.value,
+            "question": e.target.question.value,
+        };
+
+        fetch("http://jackcat.co.uk/", {mode: "no-cors", method: "POST", body: contact})
+            .then(response => {
+                console.log('hello response!', response);
+            }).catch(() => {
+                let allContacts = [];
+
+                const existingContacts = localStorage.getItem('contact');
+                if(existingContacts){
+                    allContacts = JSON.parse(existingContacts);
+                }
+
+                allContacts.push(contact);
+                localStorage.setItem('contact', JSON.stringify(allContacts));
+            });
+    };
+    askJack.addEventListener('submit', askJackFormSubmit, false);
+}
